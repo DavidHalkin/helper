bcbdaa9215a65c0883e7be1bcf9564086317f37d
David_Halkin <northfire@mail.ru>
2019-06-14 14:11:39 +0300

    sddf

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 	jcf.replaceAll();
+	 $(".link_drop_js").click(function(){
+        $(this).parent().toggleClass("active");
+        $(this).parent().find(".drop_js").slideToggle("fast");
+        return false;
+    });
+	 // favorite
+	$(".go_favorite_js a").click(function(){
+        $(this).parents(".item_task").toggleClass("in_favorite");
+        return false;
+    });
 });
