20744ab856c161f019a4d9e735eabe9b3ab264da
David_Halkin <northfire@mail.ru>
2020-01-30 17:13:23 +0200

    sdf

--- app/js/main.js
+++ app/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			$(this).parent().find(".drop_js").slideDown("fast");
 			$(this).parent().addClass("active");
 			}
-			return false;
+			//return false;
 		});
 
 	// favorite
@@ -72,4 +72,27 @@ $(document).ready(function(){
 		$(".sidebar_js").slideToggle("fast");
 		return false;
 	});
+});
+
+// custom input file
+$(function(){  
+  $('.inputfile_js').change(function(){
+    var label = $(this).parent().find('span'); 
+    if(typeof(this.files) !='undefined'){ // fucking IE      
+      if(this.files.length == 0){
+        label.removeClass('withFile').text(label.data('default'));
+      }
+      else{
+        var file = this.files[0]; 
+        var name = file.name;
+        var size = (file.size / 1048576).toFixed(3); //size in mb 
+        label.addClass('withFile').text(name + ' (' + size + 'mb)');
+      }
+    }
+    else{
+      var name = this.value.split("\\");
+	      label.addClass('withFile').text(name[name.length-1]);
+    }
+    return false;
+  });  
 });
