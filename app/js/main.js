ebdffadb4f2972454ae9a286d52c9f2d137055fb
David_Halkin <northfire@mail.ru>
2020-07-23 20:16:17 +0300

    sdf

--- app/js/main.js
+++ app/js/main.js
@@ -173,3 +173,62 @@ if ($this.hasClass('active')) {
 }
 e.preventDefault();
 });
+
+// fixed services 
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 80) {
+        $('.bar_services_js').addClass('bar_services_sticky');
+    }
+    else {
+        $('.bar_services_js').removeClass('bar_services_sticky');
+    }
+});
+
+
+// custom scroll services
+
+// Cache selectors
+var lastId,
+    topMenu = $(".bar_services_js"),
+    topMenuHeight = topMenu.outerHeight()+65,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
